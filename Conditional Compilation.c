ba54bcf939b2120408a747c4ad93b7eeda762ea1
krishnanu singh <141671865+krishnanu5836@users.noreply.github.com>
2023-08-22 20:51:46 +0530

    Add files via upload

---
+++ Conditional Compilation.c
@@ -0,0 +1,15 @@
+#define a 12
+#define b 15
+main()
+{
+    int s;
+    #if(a>b)
+        s=a+b;
+    printf("sun=%d",s);
+    #elif(a==b)
+    s=a-b;
+    printf("%d",s);
+    #endif(a<b)
+    printf("ok");
+
+}
