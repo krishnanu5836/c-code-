6f9f0794136e4ef9855ff648cd51d5306efd250f
krishnanu5836 <indu78980@gmail.com>
2023-09-01 19:51:27 +0530

    ok

---
+++ do while loop.c
@@ -0,0 +1,14 @@
+main( )
+{
+    int num,m;
+    char num2;
+    do{
+        printf("enter a num");
+        scanf("%d",&num);
+        printf("enter another");
+        scanf(" %c",&num2);
+        }while(num2=='y');
+        num++;
+
+}
+
